3e95b2dc184c53c057b14b5cf36f20a97cf38ce5
ahmerkhan15 <ahmerkhan.15@hotmail.com>
2019-03-07 16:20:12 +0500

    Cart count update on nav

--- GamingStore-SPA/src/app/services/cartservice.service.ts
+++ GamingStore-SPA/src/app/services/cartservice.service.ts
@@ -1,22 +1,32 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import {map} from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartserviceService {
-  constructor(private http:HttpClient){}
+  constructor(private http:HttpClient){this.SetCartCount();}
   baseUrl:string='http://localhost:50530/api/';
+  private cartCountSource = new BehaviorSubject(0);
+  cartCount = this.cartCountSource.asObservable();
 
-  AddToCart(model:any){
+  AddToCart(model:any):any{
     return this.http.post(this.baseUrl+'Cart/AddToCart',model).pipe(
       map((response:any)=> {
         const order = response;
         if(order){
-          console.log(order);
+          localStorage.setItem('cartCount',order.items.length);
+          this.SetCartCount();
+          return order;
         }
       })
     );
   }
+
+  SetCartCount(){
+    this.cartCountSource.next(+localStorage.getItem('cartCount'));    
+  }
+
 }
